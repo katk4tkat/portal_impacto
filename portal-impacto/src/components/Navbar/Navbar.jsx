faf3d391701096d664dd1470409ec349a9347456
pnxu <fcaavv@gmail.com>
2023-10-05 18:02:32 -0300

    refactor: Refactorizar componente Navbar y utilizar React Router para navegación en el logout

--- portal-impacto/src/components/Navbar/Navbar.jsx
+++ portal-impacto/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import "./navbar.css"
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../firebase/firebase";
+import "./navbar.css";
 
-function Navbar({ setIsLoggedIn }) {
-  const handleLogout = () => {
-    setIsLoggedIn(false);
+function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="navbar">
       <h1>PORTAL IMPACTO</h1>
       <div className="logoutbtn">
-      <h3> Cerrar Sesión</h3>
-      <button onClick={handleLogout}><img src="/Assets/logout.png"/></button>
+        <h3> Cerrar Sesión</h3>
+        <button onClick={handleLogout}>
+          <img src="/Assets/logout.png" />
+        </button>
       </div>
     </div>
   );
