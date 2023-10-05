3a5b4bf1a76769a0c0552b45d8ba7933f4c76ec8
pnxu <fcaavv@gmail.com>
2023-10-05 17:58:52 -0300

    refactor: refactorizar componente Login y usar react-router para navegación

--- portal-impacto/src/components/Login/Login.jsx
+++ portal-impacto/src/components/Login/Login.jsx
@@ -1,52 +1,41 @@
 import React, { useState } from "react";
 import { loginEmail } from "../../firebase/firebase";
-import { auth } from "../../firebase/firebase-config";
-import Navbar from "../Navbar/Navbar";
-import Home from "../Home/Home"
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
-  const submit = async () => {
+  const onSubmit = async () => {
     try {
       await loginEmail(email, password);
-      setIsLoggedIn(true);
       console.log(email, password);
+      navigate("/home");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const user = auth.currentUser;
-
   return (
     <div>
-      {!isLoggedIn ? (
-        <>
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={submit}>submit</button>
-        </>
-      ) : (
-        <>
-          <Navbar setIsLoggedIn={setIsLoggedIn} />
-          <Home/>
-        </>
-      )}
+      <>
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={onSubmit}>submit</button>
+      </>
     </div>
   );
 }
