5dff7ff0eb34e0c00ff080be9c787041188c19c9
pnxu <fcaavv@gmail.com>
2023-10-05 17:40:15 -0300

    implementa funcion isUserAuthenticated & logout de firebase/auth

--- portal-impacto/src/firebase/firebase.js
+++ portal-impacto/src/firebase/firebase.js
@@ -1,5 +1,19 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "./firebase-config.js";
 
 export const loginEmail = (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
+
+export const isUserAuthenticated = () => {
+  return auth.currentUser !== null;
+};
+
+export const logout = () => {
+  signOut(auth)
+    .then(() => {
+      console.log("logout!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
