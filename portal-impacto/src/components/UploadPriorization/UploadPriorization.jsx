b858f059337ac9e1b6445675fb42db876b764343
pnxu <fcaavv@gmail.com>
2023-10-05 18:19:05 -0300

    feat: agregar componente UploadPriorization

---
+++ portal-impacto/src/components/UploadPriorization/UploadPriorization.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./upload-priorization.css";
+
+function UploadPriorization() {
+  return (
+    <>
+      <h2>holaaaaaa</h2>
+    </>
+  );
+}
+
+export default UploadPriorization;
