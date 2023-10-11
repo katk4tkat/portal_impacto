c03752a7de41c513147f1e72657161b5515361e2
pnxu <fcaavv@gmail.com>
2023-10-11 01:05:44 -0300

    feat(Login): migrar estilos a bootstrap y agregar validación con react-hook-form

--- portal-impacto/src/components/Login/Login.jsx
+++ portal-impacto/src/components/Login/Login.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
 import { loginEmail } from "../../firebase/firebase";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const navigate = useNavigate();
 
-  const onSubmit = async () => {
+  const onSubmit = async (data) => {
     try {
-      await loginEmail(email, password);
-      console.log(email, password);
-      localStorage.setItem("userEmail", email);
+      await loginEmail(data.email, data.password);
+      localStorage.setItem("userEmail", data.email);
       navigate("/home");
     } catch (error) {
       console.log(error);
@@ -20,27 +22,73 @@ function Login() {
   };
 
   return (
-    <div className="maindiv">
-      <>
-      <div className="divlogin">
-      <h2>PORTAL IMPACTO</h2>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail"
-        />
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="******"
-        />
-        <button onClick={onSubmit} className="loginbtn">ENTRAR</button>
-        </div>
-      </>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <div
+        className="text-center"
+        style={{
+          backgroundColor: "#EBEBEB",
+          padding: "20px",
+          borderRadius: "10px",
+          width: "500px",
+          minHeight: "60vh",
+        }}
+      >
+        <h2 className="mt-5">PORTAL IMPACTO</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <Controller
+              name="email"
+              control={control}
+              rules={{ required: "Email is required" }}
+              render={({ field }) => (
+                <input
+                  type="email"
+                  {...field}
+                  className="form-control mt-5 mb-5"
+                  style={{ backgroundColor: "white" }}
+                  placeholder="E-mail"
+                />
+              )}
+            />
+            {errors.email && (
+              <span className="text-danger">{errors.email.message}</span>
+            )}
+          </div>
+
+          <div className="form-group">
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: "Password is required" }}
+              render={({ field }) => (
+                <input
+                  type="password"
+                  {...field}
+                  className="form-control mb-5 col-8"
+                  style={{ backgroundColor: "white" }}
+                  placeholder="******"
+                />
+              )}
+            />
+            {errors.password && (
+              <span className="text-danger">{errors.password.message}</span>
+            )}
+          </div>
+
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-dark btn-block mt-3 col-8"
+              style={{ width: "100%" }}
+            >
+              ENTRAR
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
