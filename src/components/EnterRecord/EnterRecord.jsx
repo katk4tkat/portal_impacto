300a6ae1d8a2d0fba83a854665b06c9c8eae0734
pnxu <fcaavv@gmail.com>
2023-11-09 12:52:25 -0300

    styles(EnterRecord): update styles

--- src/components/EnterRecord/EnterRecord.jsx
+++ src/components/EnterRecord/EnterRecord.jsx
@@ -3,34 +3,34 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import ButtonUI from "../UI/ButtonUI";
 import { useNavigate, useParams } from "react-router-dom";
-import EnterRecordForm from "./EnterRecordForm"
-
+import EnterRecordForm from "./EnterRecordForm";
 
 function EnterRecord() {
-    const navigate = useNavigate();
-    const { documentId } = useParams();
+  const navigate = useNavigate();
+  const { documentId } = useParams();
 
-    const handleReturnClick = () => {
-        navigate("/home");
-    };
+  const handleReturnClick = () => {
+    navigate("/home");
+  };
 
-    return (
-        <>
-            <Navbar />
-            <EnterRecordForm documentId={documentId} />
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-auto mt-5">
-                        <ButtonUI
-                            text="VOLVER A PRIORIZACIÓN"
-                            icon="bi bi-arrow-return-left"
-                            onClick={handleReturnClick}
-                        />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <EnterRecordForm documentId={documentId} />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto mt-5">
+            <ButtonUI
+              text="VOLVER A PRIORIZACIÓN"
+              icon="bi bi-arrow-return-left"
+              marginClassName="mb-5"
+              onClick={handleReturnClick}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default EnterRecord;
