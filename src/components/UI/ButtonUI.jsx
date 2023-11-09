d464d3f30d77fc20bfc87469be7b65cf3f4ef57f
pnxu <fcaavv@gmail.com>
2023-11-09 12:56:28 -0300

    styles(ButtonUI): update styles

--- src/components/UI/ButtonUI.jsx
+++ src/components/UI/ButtonUI.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function ButtonUI({ onClick, text, icon }) {
+function ButtonUI({ onClick, text, icon, marginClassName }) {
   return (
-    <button type="button" className="btn btn-dark" onClick={onClick}>
+    <button
+      type="button"
+      className={`btn btn-dark ${marginClassName}`}
+      onClick={onClick}
+    >
       {text}{" "}
       {icon && <i className={`bi ${icon}`} style={{ marginLeft: "4px" }}></i>}
     </button>
