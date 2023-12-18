e7662edd06bec01f0f920cf5d2002c7cb52fd44b
katk4tkat <katk4tkat@gmail.com>
2023-12-18 14:55:32 -0300

    feat(ActivityPlanning): creates ActivityPlanning component to render NavBar, Form and Return Button (ButtonUI).

--- src/components/ActivityPlanning/ActivityPlanning.jsx
+++ src/components/ActivityPlanning/ActivityPlanning.jsx
@@ -1,7 +1,33 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ActivityPlanningForm from "./ActivityPlanningForm";
+import Navbar from "../Navbar/Navbar";
+import ButtonUI from "../UI/ButtonUI";
+
 
 function ActivityPlanning() {
-  return <div>ola</div>;
+  const { documentId } = useParams()
+  const navigate = useNavigate();
+  const handleReturnClick = () => {
+    navigate("/home");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <ActivityPlanningForm documentId={documentId} />
+      <div className="d-flex justify-content-center mt-5">
+        <ButtonUI
+          text="VOLVER"
+          icon="bi bi-arrow-return-left"
+          marginClassName="mb-5"
+          btnClassName="btn-link"
+          onClick={handleReturnClick}
+        />
+      </div>
+    </>
+  )
 }
 
 export default ActivityPlanning;
